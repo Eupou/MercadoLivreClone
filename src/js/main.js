424e68d712b23ec2031fc1bb0bd02d7dd40ae9ab
Wesley Gomes <wesleywesleygomesgomes@gmail.com>
2025-12-05 19:53:47 -0300

    refactor: change folder structure

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,11 @@ function updateMediaQuery() {
   let logoImg = document.getElementById("logoImg")
   let searchInput = document.getElementById("searchBarInput")
   if (window.matchMedia("(min-width: 800px)").matches) {
-    logoImg.src = "./imgs/logo.webp"
+    logoImg.src = "../../src/imgs/logo.webp"
     searchInput.placeholder = "Buscar produtos, marcas e muito mais"
   }
 }
 
 updateMediaQuery()
+
+console.log("hello")
