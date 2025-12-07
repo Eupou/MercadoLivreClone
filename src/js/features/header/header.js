31ee90be267fc7f3cb2654906981144cfc477717
Wesley Gomes <wesleywesleygomesgomes@gmail.com>
2025-12-07 18:06:06 -0300

    fix: change image path

--- src/js/features/header/header.js
+++ src/js/features/header/header.js
@@ -3,7 +3,7 @@ let searchInput = document.getElementById("searchBarInput")
 
 function updateMediaQuery() {
   if (window.matchMedia("(min-width: 800px)").matches) {
-    logoImg.src = "../../src/imgs/logo.webp"
+    logoImg.src = "./src/imgs/logo.webp"
     searchInput.placeholder = "Buscar produtos, marcas e muito mais"
   }
 }
